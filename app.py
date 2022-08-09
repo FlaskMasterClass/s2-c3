edf2d4b92d7591f2126d416465a6b66710c19702
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-08-09 09:25:03 +0400

    chore(gitignore): add

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+
+import click
+import flask
+from flask import Flask
+from flask import current_app
+from flask import redirect
+from flask import render_template
+from flask.cli import load_dotenv
+from flask_login import AnonymousUserMixin
+from flask_login import UserMixin
+
+from auth_views import auth_blueprint
+from secret_views import secret_blueprint
+from init import db
+from init import login_manager
+from init import migrate
+from models import User
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+from config import DevelopmentConfig
+from config import ProductionConfig
+from config import TestingConfig
+
+profiles = {
+    'development': DevelopmentConfig(),
+    'production': ProductionConfig(),
+    'testing': TestingConfig()
+}
+
+def create_app(profile):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(profiles[profile])
+    app.config.from_pyfile("config.py", silent=True)
+
+
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(secret_blueprint)
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    if profile != "testing":
+        app.config.from_pyfile("config.py", silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.shell_context_processor
+    def shell():
+        return {
+            "db": db,
+            "User": User
+        }
+
+
+    @app.route('/')
+    def home():
+        return render_template('home.html')
+
+    return app
+
+
+flask_env = os.environ.get("FLASK_ENV", default="development")
+app = create_app(flask_env)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5005, debug=True, ssl_context='adhoc')
